8d05a3c42575dd42a9b547d98702272807b21c4b
Prima Aulia Gusta <primaulia@gmail.com>
2017-04-13 16:36:52 +0800

    setup for facebook auth

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,7 @@
 var passport = require('passport')
 var LocalStrategy = require('passport-local').Strategy
+var FacebookStrategy = require('passport-facebook').Strategy
+
 var User = require('../models/user')
 
 // set the authenticated user data into session
@@ -17,14 +19,23 @@ passport.deserializeUser(function (id, done) {
   })
 })
 
-// passport middleware strategy
+// passport facebook strategy
+passport.use('facebook', new FacebookStrategy({
+  clientID: process.env.FB_APP_KEY,
+  clientSecret: process.env.FB_APP_SECRET,
+  callbackURL: 'http://localhost:3300/auth/facebook/callback',
+  enableProof: true,
+  profileFields: ['name', 'emails']
+}, function (accessToken, refreshToken, profile, next) {
+  console.log('authenticating using facebook strategy')
+}))
+
+// passport local-signup strategy
 passport.use('local-signup', new LocalStrategy({
   usernameField: 'email',
   passwordField: 'password',
   passReqToCallback: true
 }, function (req, givenEmail, givenPassword, next) {
-  console.log('im running passport authentication here')
-  console.log(req.body)
   var newUser = new User({
     email: givenEmail,
     name: req.body.name,
